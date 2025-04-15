06d8bcabb5701acb98502a0e05874ae24613cf39
mixalis20 <mtsatsakoulias@gmail.com>
2025-04-15 08:55:54 +0300

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,38 +1,14 @@
 // src/components/Navbar.jsx
-import React, { useEffect } from 'react';
+import React from 'react';
 
 
 const Navbar = () => {
-  useEffect(() => {
-    const darkMode = localStorage.getItem('darkMode');
-    if (darkMode === 'enabled') enableDarkMode();
-  }, []);
-
-  const toggleDarkMode = () => {
-    if (document.body.classList.contains('dark-mode')) {
-      disableDarkMode();
-    } else {
-      enableDarkMode();
-    }
-  };
-
-  const enableDarkMode = () => {
-    document.body.classList.add('dark-mode');
-    localStorage.setItem('darkMode', 'enabled');
-  };
-
-  const disableDarkMode = () => {
-    document.body.classList.remove('dark-mode');
-    localStorage.setItem('darkMode', 'disabled');
-  };
-
   return (
     <div className="navbar">
       <ul>
-        <li><a href="index.html">Image</a></li>
-        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="index.html" id="imageLink">Image</a></li>
+        <li><a href="gallery.html" id="galleryLink">Gallery</a></li>
       </ul>
-      
     </div>
   );
 };
