69574269f23383ec05564a124adcae929232b512
GEOVOUT06 <g.voutidis@gmail.com>
2025-06-10 16:03:49 +0300

    csproj correct version

--- backend/obj/Debug/net6.0/App.AssemblyInfo.cs
+++ backend/obj/Debug/net6.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa4183fc031afb642d255fa93d2272ab5504e9d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9256a9b07bbc586f3402ffc24c41f1b2d2c60c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
